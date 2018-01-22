79383094dcd3348cfb3244cb01fb28c0ba334c65
Heike Hofmann <hofmann@iastate.edu>
2018-01-21 22:10:51 -0600

    added numbers for cities in Iowa

--- R/data.R
+++ R/data.R
@@ -38,3 +38,27 @@
 #'   \item{Motor.vehicle.theft}{number of reported motor vehicle thefts.}
 #' }
 "fbiwide"
+
+#' Numbers of crimes by city
+#'
+#' A dataset containing the number of property and violent crimes across Iowa from 2006 to 2016. 
+#' The data was made available by the FBI in the Uniform Crime Reporting Statistics (UCR) at \url{https://www.ucrdatatool.gov/index.cfm}. The variables are as follows (more detail on the FBI website):
+#'
+#' @format A data frame with 1207 rows and 14 variables:
+#' \describe{
+#'   \item{City}{name of the city for which numbers are reported.}
+#'   \item{Population}{state population.}
+#'   \item{Violent.crime}{number of reported violent crimes.}
+#'   \item{Murder}{number of reported murders.}
+#'   \item{Rape}{number of reported rapes.  The definition of rape was updated in 2012. Numbers from 2013 reflect the new definition.}
+#'   \item{Robbery}{number of reported robberies.}
+#'   \item{Aggravated.assault}{number of reported aggravated assaults.}
+#'   \item{Property.crime}{total number of reported property crimes.}
+#'   \item{Burglary}{number of reported burglaries.}
+#'   \item{Larceny.theft}{number of reported larceny thefts.}
+#'   \item{Motor.vehicle.theft}{number of reported motor vehicle thefts.}
+#'   \item{Arson}{number of reported cases of arsons.}
+#'   \item{State}{name of the state for which numbers are reported.}
+#'   \item{Year}{year of the reporting.}
+#' }
+"cities"
